af57ce2e48751dce4b211b22e456f4eb25a704f6
TonghuiLi10 <112099944+TonghuiLi10@users.noreply.github.com>
2022-09-08 10:36:46 -0500

    final version

--- bonus_quiz.R
+++ bonus_quiz.R
@@ -1,9 +1,11 @@
 # 1) Generate 50 observations from a Binomial distribution with m = 20 and p = 0.5 and assign it to an object called `fish`.
-fish<-rbinom(n=50, size=20, prob=0.5) 
+fish<-rbinom(n=50, size=20, prob=0.5)
 fish
 # 2) Take the square root of `fish` and multiply it by the logarithm of 10.
 sqfish<-sqrt(fish)
-answer2<-10*sqfish
+sqfish
+answer2<-log10(sqfish)
+answer2
 # 3) Create a histogram of 100 observations from a Gamma distribution with shape parameter = 2. Use the function `hist()`.
 set.seed(0)
 answer3<-rgamma(n=100, shape=2)
@@ -13,11 +15,12 @@ hist(answer3)
 answer4<-subset(answer3,answer3>2)
 answer4
 length(answer4)
-probability<-37/100
+probability<-length(answer4)/100
 probability
 # 5) Install the package `idar` from the GitHub repository "https://github.com/SMAC-Group/idar" (if you have an install error, copy and paste it for your response).
 install.packages("devtools")
 devtools::install_github("SMAC-Group/idar")
+
 # 6) Install the simts package from CRAN using code (i.e. do not use the RStudio tabs).
 install.packages("simts")
 library("simts")
@@ -41,10 +44,10 @@ gen_gts(
 model = AR1(phi = .5, sigma2 = .1) + WN(sigma2=1)
 x = gen_gts(n, model)
 plot(x)
-newx<-as.vector(x)
-newx
+x<-as.vector(x)
+x
 # 8) Subset observations in this new 'x' to extract only positive observations and name this subset 'x_pos'.
-x_pos<-subset(newx,newx>0)
+x_pos<-subset(x,x>0)
 x_pos
 # 9) Modify the same code from Question 7 to generate a vector of length n = 100.
 set.seed(1336)
@@ -73,6 +76,10 @@ day <- round(runif(100, 1, 28))
 month <- round(runif(100, 1, 12))
 dates <- apply(cbind(day, month, year), 1, paste, collapse = "/")
 dates
-answer10<-as.Date(dates)
+answer10<-as.Date(dates,"%d/%m/%Y")
+answer10
 # Optional: Create a plot of the object `x` generated in Question 10 where the y-axis represents the values of `x` and the x-axis represents the `dates` object formatted correctly. Use the plot() function with the argument `type = "l"`.
-plot()
+optional<-
+plot(optional,type = "l")
+
+
